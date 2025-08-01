7d257383d147221a971e0c042b47c306492b2e0a
SergiyVitruk <vitruk.sergiy@gmail.com>
2025-08-02 02:04:32 +0300

    ready task

---
+++ src/services/movieServices.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import type { Movie, SearchResponse } from "../types/movie";
+
+const myToken = import.meta.env.VITE_TMDB_TOKEN;
+
+export const fetchMovies = async (query: string): Promise<Movie[]> => {
+  const response = await axios.get<SearchResponse>(
+    `https://api.themoviedb.org/3/search/movie`,
+    {
+      params: {
+        query,
+        language: "en-US",
+        page: 1,
+      },
+      headers: {
+        Authorization: `Bearer ${myToken}`,
+        accept: "application/json",
+      },
+    }
+  );
+  return response.data.results;
+};
